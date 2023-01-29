65a015556171e9960452247b636f79c41a804972
HP <johanna261michael@gmail.com>
2023-01-29 18:23:40 -0400

    Initial commit

---
+++ app/src/main/java/edu/ucne/tepresto/MainActivity.kt
@@ -0,0 +1,35 @@
+package edu.ucne.tepresto
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
+import edu.ucne.tepresto.ui.ocupacion.OcupacionScreen
+import edu.ucne.tepresto.ui.theme.TePrestoTheme
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TePrestoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    OcupacionScreen()
+                }
+            }
+        }
+    }
+}
+
